29c906f3f782ebfb958598af1fa8ab9305d7f638
Simon Schauer <simon.schauer37@gmail.com>
2025-10-27 10:06:33 +0100

    test

---
+++ simon.sh
@@ -0,0 +1,5 @@
+GIT_AUTHOR_NAME="Simon Schauer" \
+GIT_AUTHOR_EMAIL="simon.schauer37@gmail.com" \
+GIT_COMMITTER_NAME="Simon Schauer" \
+GIT_COMMITTER_EMAIL="simon.schauer37@gmail.com" \
+git commit 
